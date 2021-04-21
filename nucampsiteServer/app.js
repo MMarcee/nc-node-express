badec38691a4a472685d0b41126a094ce332dc79
MMarcee <marbouame@gmail.com>
2021-04-21 09:13:06 -0400

    Basic Authentication

--- nucampsiteServer/app.js
+++ nucampsiteServer/app.js
@@ -36,6 +36,36 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// @Authentication Middleware to serve static files:
+// The authentication being placed above the app.use(express.static(path.join(__dirname, 'public'))); forces user to authenticate before having access to the static files.
+// Placing the authentication below the app.use(express.static(path.join(__dirname, 'public'))); allows user to have access to the static files without authenticating.
+function auth(req, res, next) {
+  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+  if (!authHeader) {
+    const err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+
+  const auth = Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':');
+  const user = auth[0];
+  const pass = auth[1];
+  if (user === 'admin' && pass === 'password') {
+    return next(); // authorized
+  } else {
+    const err = new Error('You are not authenticated!');
+    res.setHeader('WWW-Authenticate', 'Basic');
+    err.status = 401;
+    return next(err);
+  }
+  
+}
+
+app.use(auth);
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
