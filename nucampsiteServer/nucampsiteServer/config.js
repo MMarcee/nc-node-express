e2e6b75f91bd11daa345f12695b461c6e5f1ef24
MMarcee <marbouame@gmail.com>
2021-05-02 18:36:27 -0400

    Update config.js

--- nucampsiteServer/nucampsiteServer/config.js
+++ nucampsiteServer/nucampsiteServer/config.js
@@ -1,8 +1,1 @@
-module.exports = {
-    'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/nucampsite', 
-    'facebook': {
-        clientId: '260174915845613',
-        clientSecret: 'b18d098b314f65d40da5292dec869ab8'
-    }
-}
+
